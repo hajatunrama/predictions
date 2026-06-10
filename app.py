1ea25dbcf95d6d4539b7017656e22a3ee6291f29
hajatunrama <abdulhajatun47@gmail.com>
2026-06-10 09:17:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import pickle
 import pandas as pd
 
 # Load model dan dataframe
-pipe = pickle.load(open('pipe.pkl', 'rb'))
-#df = pickle.load(open('df.pkl', 'rb'))
+# pipe = pickle.load(open('pipe.pkl', 'rb'))
+df = pickle.load(open('df.pkl', 'rb'))
 
 st.title("Prediksi Harga Laptop 💻")
 st.write("Masukkan spesifikasi laptop di bawah ini untuk mengetahui estimasi harganya.")
